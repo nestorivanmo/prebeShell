c2cc98718ccae226a6f35d8b543d7b9d4cf51367
nestorivanmo <nestor.martinez.98@hotmail.com>
2018-02-25 13:57:44 -0600

    script del comando manpage -> falta formato

---
+++ manpage.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+echo "l creada por Daniel Bárcenas, Néstor Martínez y Daniel Pineda"
+echo "La lista de comandos es la siguiente:"
+
+echo "man page -> despliega información sobre los comandos"
+echo "juego 1 -> descripción juego1"
+echo "juego2 -> descripción juego 2"
+echo "juego3 -> descripción juego3"
+
+echo "prebeplayer -> reproductor de musica"
+echo "salir -> sale de la prebeshell"
+echo "arbol -> nos puestra la jerarquía de un directorio"
+echo "fecha -> nos despliega la fecha"
+echo "hora -> nos despliega la hora"
+
+echo "buscar -> este comando recibe dos argumentos: archivo a buscar y directorio donde se va a buscar"
+ 

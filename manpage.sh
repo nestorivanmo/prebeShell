1df4256b49fc504e40b0f035cedfeffb62318efa
nestorivanmo <nestor.martinez.98@hotmail.com>
2018-02-25 14:44:00 -0600

    comando manpage listo

--- manpage.sh
+++ manpage.sh
@@ -1,18 +1,34 @@
-#!bin/bash
+#!/bin/bash
+clear
+echo " "
+echo "                                                      COMANDOS PREBESHELL"
+echo " "
 
-echo "l creada por Daniel Bárcenas, Néstor Martínez y Daniel Pineda"
-echo "La lista de comandos es la siguiente:"
+echo "PAGE"
+echo "       Lista de comandos utilizables en la prebeshell"
+echo " "
 
-echo "man page -> despliega información sobre los comandos"
-echo "juego 1 -> descripción juego1"
-echo "juego2 -> descripción juego 2"
-echo "juego3 -> descripción juego3"
+echo "DESCRIPCIÓN"
+echo " "
+echo "    man page -> despliega información sobre los comandos"
+echo "    juego 1 -> descripción juego1"
+echo "    juego2 -> descripción juego 2"
+echo "    juego3 -> descripción juego3"
 
-echo "prebeplayer -> reproductor de musica"
-echo "salir -> sale de la prebeshell"
-echo "arbol -> nos puestra la jerarquía de un directorio"
-echo "fecha -> nos despliega la fecha"
-echo "hora -> nos despliega la hora"
+echo "    prebeplayer -> reproductor de musica"
+echo "    salir -> sale de la prebeshell"
+echo "    arbol -> nos puestra la jerarquía de un directorio"
+echo "    fecha -> nos despliega la fecha"
+echo "    hora -> nos despliega la hora"
+echo "    buscar -> este comando recibe dos argumentos: archivo a buscar y directorio donde se va a buscar"
 
-echo "buscar -> este comando recibe dos argumentos: archivo a buscar y directorio donde se va a buscar"
- 
+echo " "
+
+echo "AUTORES"
+
+echo "    Erick Bárcenas, Néstor Martínez y Daniel Pineda"
+
+echo " "
+echo "Presiona cualquier tecla para continuar..."
+read
+

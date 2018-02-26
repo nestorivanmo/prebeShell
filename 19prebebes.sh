ec933ab8d47fa69e77538e1f63fa6c0e2f609a45
barcenitas <erick3arcenas7@gmail.com>
2018-02-26 02:25:55 -0600

    19prebebes es un juego

---
+++ 19prebebes.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+clear
+sleep 1s
+figlet "19 Prebebes"
+
+echo -ne '                                             (0%)\r'
+sleep 1 
+clear
+figlet "19 Prebebes"
+echo -ne '##################                           (20%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '####################                         (50%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '##########################                   (80%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '##############################               (100%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '##########################                   (80%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '####################                         (50%)\r'
+sleep 1 
+clear
+figlet "19 Prebebes"
+echo -ne '##############                               (35%)\r'
+
+echo -ne '\n'
+
+sleep 1s
+echo 
+

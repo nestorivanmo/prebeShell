28061ecb95832881c6e3a818803de221022993e0
barcenitas <erick3arcenas7@gmail.com>
2018-03-03 18:52:50 -0600

    nueva barra de progreso y bordes del juego

--- 19prebebes.sh
+++ 19prebebes.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
+BORDE="░"
+MARGENSUP=1
+MARGENIZQ=10 
+UCOL=50
+UFIL=40
 
+prebes(){
 clear
 sleep 1s
 figlet "19 Prebebes"
 
-echo -ne '                                             (0%)\r'
+echo -ne '                                                  (0%)\r'
 sleep 1 
 clear
 figlet "19 Prebebes"
-echo -ne '##################                           (20%)\r'
-sleep 1
-clear
-figlet "19 Prebebes"
-echo -ne '####################                         (50%)\r'
-sleep 1
-clear
-figlet "19 Prebebes"
-echo -ne '##########################                   (80%)\r'
-sleep 1
-clear
-figlet "19 Prebebes"
-echo -ne '##############################               (100%)\r'
-sleep 1
-clear
-figlet "19 Prebebes"
-echo -ne '##########################                   (80%)\r'
-sleep 1
-clear
-figlet "19 Prebebes"
-echo -ne '####################                         (50%)\r'
-sleep 1 
-clear
-figlet "19 Prebebes"
-echo -ne '##############                               (35%)\r'
 
-echo -ne '\n'
+echo -ne '😂 😅 😂 😇 🙂 🙃 😉 😌 😍 😘 😗                             (20%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '😶 😐 😑 😯 😦 😧 😮 😲 😵 👩 ‍👩 ‍👦 ‍👦                         (50%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '👩 ‍🌾 👨 ‍🌾 👩 ‍🍳 👨 ‍🍳 👩 ‍🎓 👨 ‍🎓 👩 ‍🎤 👨 ‍🎤                   (80%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '👿 👹 👺 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 🙎 🙎 ‍♂️ 🙍 🙍 ‍♂️         (100%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '👨 ‍👩 ‍👧 👨 ‍👩 ‍👧 ‍👦 👨 ‍👩 ‍👦 ‍👦 👨 ‍👩 ‍👧‍ 👧 👩 ‍👩 ‍👦               (80%)\r'
+sleep 1
+clear
+figlet "19 Prebebes"
+echo -ne '😭 😓 😪 😴 🙄 😩 😤 😠 😡 😳 😱 😨 😰 😢                       (50%)\r'
+sleep 1 
+clear
+figlet "19 Prebebes"
+echo -ne '😀 😃 😄 😁 😆 🤓 😎                                     (35%)\r'
+
 sleep 1s
 # VAMOS A PONERLE COLOR A UN CODIGO DE  LA HISTORIA
 
@@ -62,16 +68,86 @@ cecho blue  " Dic. 2017.
   40 de ellos llegaron el priemer dia,y 30 al segundo,
   conforme pasaron los dias fueron desertando.
   Hoy solo quedan 19 prebebes para resonar en la eternidad"
+
+cecho cyan " Los prebebes restantes jugaban con un globo
+  este se le escapo a un prebebe. Tu mision es dirigirlos
+  para construir una torre,que ayude a llegar hasta el globo"
 echo '\n'
 
-echo "Jugaras con el Prebebe:"
-shuf  -n 1 -i 0-30
-#echo  '\n' 
-echo "¿Deseas saber sus poderes?"
-echo '\n' 
+
+PREBE=`echo $(($RANDOM%22))`
 
 
-printf "La pregunta"
-if [[ $(yesno no) == yes ]]; then
-        #lo que sea que queramos hacer
-fi
+echo "Jugaras con el Prebebe $PREBE:"
+
+#echo  '\n'
+}
+
+
+
+bordes(){
+tput setf 6
+   tput cup $MARGENSUP $MARGENIZQ
+   x=$MARGENIZQ
+   while [ "$x" -le "$UCOL" ];
+   do
+      printf %b "$BORDE"
+      x=$(( $x + 1 ));
+   done
+
+   # Draw sides
+   x=$MARGENSUP
+   while [ "$x" -le "$UFIL" ];
+   do
+      tput cup $x $MARGENIZQ; printf %b "$BORDE"
+      tput cup $x $UCOL; printf %b "$BORDE"
+      x=$(( $x + 1 ));
+   done
+
+   # Draw bottom
+   tput cup $UFIL $MARGENIZQ
+   x=$MARGENIZQ
+   while [ "$x" -le "$UCOL" ];
+   do
+      printf %b "$BORDE"
+      x=$(( $x + 1 ));
+   done
+   tput setf 9
+
+echo -e " \n " 
+
+}
+
+ZEROES=`echo |awk '{printf("%0"'"$SNAKESIZE"'"d\n",$1)}' | sed 's/0/0 /g'`
+LASTPOSX=( $ZEROES )                    # Pad with zeroes to start with
+LASTPOSY=( $ZEROES )                    # Pad with zeroes to start with
+
+#SCORE=0                                 # Starting score
+
+clear
+prebes
+echo "
+Keys:
+
+ W - ARRIBA
+ S - ABAJO
+ A - IZQUIERDA
+ D - DERECHA
+ X - SALIR
+
+
+
+
+Press Return to continue
+"
+stty -echo
+tput civis
+read RTN
+tput setb 0
+tput bold
+clear
+
+bordes
+#updatescore 0
+
+

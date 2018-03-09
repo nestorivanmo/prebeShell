36cd1d2bb812bf0961de38bc2b8e0c052cfec6a3
barcenitas <erick3arcenas7@gmail.com>
2018-03-09 08:46:15 -0600

    funciones color

--- funciones.sh
+++ funciones.sh
@@ -1,31 +1,102 @@
 #!/bin/bash
+emperador(){
+
+echo -e "${GREEN}
+
+
+						                               {{E}}
+						               <>               |.|
+						             .::::.             |M|
+						         @\\/W\/\/W\//@          |.|
+						          \\/^\/\/^\//     _    )P(    _
+						           \_O_<>_O_/     (@)__/ . \__(@)
+						      ____________________  ~ -=)E(=- ~ 
+					             |<><><>  |  |  <><><>|     |.|
+						     |<>      |  |      <>|     |R|
+					   	     |<>   PREBESHELL   <>|     | |
+						     |<>   .--------.   <>|     |.|
+			  			     |     |   ()   |     |     |A|
+						     |____|0 LINUX 0 |____|     | |
+						     |     \   /\   /     |     |.|
+						     |------\  \/  /------|     |D|
+						     |   TITULAR: QUIÑO   |     | |
+						     |        |  |        |     |.|
+						     :        2018        :     |O|
+						      \<>     |  |     <>/      | |
+						       \<>    |  |    <>/       |.|
+						        \<>   |  |   <>/        |R|
+						          \<> |  | <>/          | |
+						            -.|  |.-            \ /
+						               --                ^
+";
+}
+
+
+
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+STD='\033[0;0;39m'
+CYAN='\033[1;96m'
+IBlUE='\033[0;104m'
+WHITE="\033[1;97m"
+BLUE='\033[1;34m'
+
 errorSignal()
 {
- echo " intenta usar el comando 'salir'"
+ echo -e " ${GREEN} Intenta usar el comando: ' ${WHITE} salir'"
 }
 
 mostrarFecha()
 {
- echo $(date +"%a %d/%m/%y")
- echo "Día: "$(date +"%j")
+ echo -e $(date +"${WHITE}%a ${GREEN}%d/${YELLOW}%m/${GREEN}%y")
+ echo -e "${BLUE}Día: "$(date +"${CYAN}%j")
 }
 mostrarHora()
 {
- echo $(date +"%H:%M")
+ echo -e $(date +" ${YELLOW}%H:${WHITE}%M")
 }
 
 trap 'errorSignal' SIGINT SIGTSTP
 menu() {
- clear
- echo -e "\t\t \033[0;0;39m Bienvenido, eres de nuevo ingreso a esta prebeshell"
-sleep 1
- echo -e "\t\t\t ...................." 
- echo -e "Esta es una gaid fast de todo lo que tienes que saber para comenzar"
 sleep 1
  clear
+hola(){
+clear
+ echo ""
+ echo ""
+ echo ""
+ echo ""
+ echo ""
+ echo -e " 		${GREEN}  ______     __     ______     __   __     __   __   ______     __   __     __     _____     ______    "
+ echo -e "		${GREEN} /\  == \   /\ \   /\  ___\   /\  -.\ \   /\ \ / /  /\  ___\   /\  -.\ \   /\ \   /\  __-.  /\  __ \   "
+ echo -e "		${YELLOW} \ \  __<   \ \ \  \ \  __\   \ \ \-.  \  \ \ \'/   \ \  __\   \ \ \-.  \  \ \ \  \ \ \/\ \ \ \ \/\ \  "
+ echo -e "		${RED}  \ \_____\  \ \_\  \ \_____\  \ \_\\ \_\  \ \__|    \ \_____\  \ \_\\ \_\  \ \_\  \ \____-  \ \_____\ "
+ echo -e "		${CYAN}   \/_____/   \/_/   \/_____/   \/_/ \/_/   \/_/      \/_____/   \/_/ \/_/   \/_/   \/____/   \/_____/ "
+                                                                                                      
+echo ""
+echo ""
+echo ""
 
- echo -e "Ponte trucha que se quitarán de volón pinpón"
+emperador
+}
+hola
+echo -e "	                              ${WHITE} Eres de nuevo ingreso a esta Prebeshell EMPERADOR"
+
+sleep 2
+ echo -e "      	▲ ▼ ▶ ◀ ▼ ▲ ▲ ▼ ▶ ▼ ▲ ▲ ▼ ▶ ◀ ▶ ▲ ▲ ▶ ▶ ▼ ▲ ▶ ▶ ▶ ▼ ▲ ◀ ◀ ▶ ▼ ◀ ▶ ▼ ▶ ▶ ◀ ▼ ▲ ◀ ▶ ▲ ▼ ▶ ▶ ▼ ▼ ▲ ◀ ▶ ▼ ▲ " 
+hola
+ echo -e "				${BLUE} Esta es una gaid fast de todo lo que tienes que saber para tener éxito"
+sleep 2
+hola
+
+echo -e "						${RED} Ponte trucha que se quitarán de volón pinpón"
+sleep 1s
+echo -e "	      ▲ ▼ ▶ ◀ ▼ ▲ ▲ ▼ ▶ ▼ ▲ ▲ ▼  ▶ ◀ ▶ ▲ ▲ ▶ ▶ ▼ ▲ ▶ ▶ ▶ ▼ ▲ ◀ ◀ ▶ ▼ ◀ ▶ ▼ ▶ ▶ ◀ ▼ ▲ ◀ ▶ ▲ ▼ ▶ ▶ ▼ ▼ ▲ ◀ ▶ ▼ ▲ " 
+clear
 sleep 1
+
  ./manpage.sh
 sleep 1
 }
@@ -34,15 +105,24 @@ menu
 
 set SCRIPT='readlink "$0"'
 PS1=$(hostname -f)" @ "$PWD
-CERO=0
 
 while :
 do
- echo -n $PS1": "
+ echo -n $PS1" : "
  read COMMAND_INPUT args
  case $COMMAND_INPUT$args in
-	(alv|salir) clear; echo -e "\033[33m \n\n\n\n\n \t\t\t  Láaaaaaastima que terminó el feeeestival de hoooy :,( . Weno ni fart, bais";sleep 1; echo -e "Cerrando procesos . . ."; sleep 1;
-	echo -e "Volviendo a tu shell sencilla y aburrida . . ."; sleep 1; echo -e "¡LISTO! Bai final" sleep 1; clear; 
+	(alv|salir) clear; echo -e "${YELLOW} \n\n\n\n\n \t\t\t  Láaaaaaastima que terminó el feeeestival de hoooy :,( . Weno ni fart, bais";
+sleep 1; 
+
+echo -e "							${BLUE}Cerrando procesos . . ."; 
+sleep 1;
+	echo -e "					${RED}Volviendo a tu shell sencilla y aburrida . . .";
+ sleep 1;
+ echo -e "							${GREEN}¡LISTO! Bai final" 
+
+
+sleep 1; clear; 
+	echo -e "      ▲ ▼ ▶ ◀ ▼ ▲ ▲ ▼ ▶ ▼ ▲ ▲ ▼ ▶ ◀ ▶ ▲ ▲ ▶ ▶ ▼ ▲ ▶ ▶ ▶ ▼ ▲ ◀ ◀ ▶ ▼ ◀ ▶ ▼ ▶ ▶ ◀ ▼ ▲ ◀ ▶ ▲ ▼ ▶ ▶ ▼ ▼ ▲ ◀ ▶ ▼ ▲  ▼ ▶ ▶ ◀ ▼ ▲ ◀ " 
 	echo -e "\n\n\n\n\n          CCCCCCCCCCCCCCCC        IIIIIIIIIIIIIIIIIIIIIII		AAAA		         	OOOOOOOOOOOO"
 	echo -e "         CCCCCCCCCCCCCCCCC        IIIIIIIIIIIIIIIIIIIIII               AAAA   AAAA                       OOOOOOOOOOO"
 	echo -e "        CCCCCCCCCCCCCCCCC           IIIIIIIIIIIIIIIIII               AAAA      AAAAA                   OOOOOOOOOO OOOOOOO"
@@ -65,7 +145,7 @@ do
 	echo -e "        CCCCCCCCCCC                     IIIIIIIIIIII             AAAAAA            AAAA         OOOOOOOOOOOOOOOOOOOOOOOOOO"
 	echo -e "         CCCCCCCCCC                     IIIIIIIIIIII             AAAAAA            AAAA            OOOOOOOOOOOOOOOOOOOOO"
 	echo -e "          CCCCCCCCCCCCCCC            IIIIIIIIIIIIIIIII           AAAAAA            AAAA            OOOOOOOOOOOOOOOOOOOO"
-	echo -e "           CCCCCCCCCCCCCCC          IIIIIIIIIIIIIIIIIIII         AAAAAA            AAAA              OOOOOOOOOOOOOOOOO"
+	echo -e "echo -e "      ▲ ▼ ▶ ◀ ▼ ▲ ▲ ▼ ▶ ▼ ▲ ▲ ▼ ▶ ◀ ▶ ▲ ▲ ▶ ▶ ▼ ▲ ▶ ▶ ▶ ▼ ▲ ◀ ◀ ▶ ▼ ◀ ▶ ▼ ▶ ▶ ◀ ▼ ▲ ◀ ▶ ▲ ▼ ▶ ▶ ▼ ▼ ▲ ◀ ▶ ▼ ▲ "            CCCCCCCCCCCCCCC          IIIIIIIIIIIIIIIIIIII         AAAAAA            AAAA              OOOOOOOOOOOOOOOOO"
 		sleep 2; clear
 		exit;;
 	(pp) ./prebePlayer.sh;;
@@ -75,7 +155,7 @@ do
 	(deit|fecha) mostrarFecha;;
 	(mozek) ./memoria.sh;;
 	(arbol) ./arbol.sh;;
-	*) echo $COMMAND_INPUT": comando inváido";;
+	*) echo $COMMAND_INPUT":${RED} Comando inváido";;
  esac
 done
 

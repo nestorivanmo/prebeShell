56156aeea900cba11f25498ca2aad58632ffc133
barcenitas <erick3arcenas7@gmail.com>
2018-03-09 04:34:51 -0600

    19prebesbes juego 1 98%

--- 19prebebes.sh
+++ 19prebebes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BORDE="░"
+#BORDE="░"
 MARGENSUP=1
 MARGENIZQ=10 
 UCOL=50
@@ -7,7 +7,7 @@ UFIL=40
 
 clear
 prebes(){
-
+#<<COMMENT
 bannner(){
 clear
 echo -e  "  "
@@ -74,6 +74,8 @@ echo -ne ' 		         		  😀 😃 😄 😁 😆 🤓 😎                    
 sleep 1s
 # VAMOS A PONERLE COLOR A UN CODIGO DE  LA HISTORIA
 
+#COMMENT
+
 echo -e  "  "
 echo -e  "  "
 echo -e  "  "
@@ -81,22 +83,6 @@ echo -e  "  "
 echo -e  "  "
 echo -e  "  "
 
-cecho() {
-  local code="\033["
-  case "$1" in
-    black  | bk) color="${code}0;30m";;
-    red    |  r) color="${code}1;31m";;
-    green  |  g) color="${code}1;32m";;
-    yellow |  y) color="${code}1;33m";;
-    blue   |  b) color="${code}1;34m";;
-    purple |  p) color="${code}1;35m";;
-    cyan   |  c) color="${code}1;36m";;
-    gray   | gr) color="${code}0;37m";;
-    *) local text="$1"
-  esac
-  [ -z "$text" ] && local text="$color$2${code}0m"
-  echo "$text"
-}
 clear 
 
 echo -e  "  "
@@ -107,117 +93,66 @@ echo -e  "  "
 echo -e  "  "
 echo -e  "  "
 echo -e  "  "
-echo -e  "  "
-echo -e  "  "
-echo -e  "  "
-echo -e  "  "
-echo -e  "  "
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+STD='\033[0;0;39m'
+CYAN='\033[1;96m'
+
+
+
+echo -e " ${GREEN} 							|_|~|~(~~|~/~\|~)~|~ /\   ${STD}  "
+echo -e " ${GREEN}							| |_|__) | \_/|~\_|_/~~\  ${STD}"
+
+echo ""
+                                echo "" 
+echo ""
+                                echo "" 
 #AQUI TERMINA EL CODIGO DE COLOR
-				cecho blue  " Dic. 2017. 
-				       	      Ochenta prebebes fueron a la prueba de conocimientos,
-				              solo 55 fueron aceptados.
-  					      40 de ellos llegaron el priemer dia,y 30 al segundo,
-  					      conforme pasaron los dias fueron desertando.
-  					      Hoy solo quedan 19 prebebes para resonar en la eternidad"
+				echo -e        "					${YELLOW}¡Dic. 2017! ${STD}" && sleep 1
+				echo        "			      	         Ochenta prebebes fueron a la prueba de conocimientos,"&& sleep 1
+				echo        "		          	         solo 55 fueron aceptados. "&& sleep 1
+  				echo	    "		  			 40 de ellos llegaron el priemer dia,y 30 al segundo, "&& sleep 1
+  				echo	    "		  			 y conforme pasaron los dias fueron desertando. " && sleep 1
 
-				 cecho cyan " Los prebebes restantes jugaban con un globo
-  					      este se le escapo a un prebebe. Tu mision es dirigirlos
-				              para construir una torre,que ayude a llegar hasta el globo"
-echo '\n'
+  				echo ""
+				echo ""	      
+				echo -e        "		                        ${YELLOW}¡FEBRERO 2018! ${STD}" && sleep 1
+
+				echo        "		 	         	 Los prebebes restantes fueron enloqueciendo por la"&& sleep 1
+  				echo	    "			                abismales tareas."&& sleep 1
+				echo -e	    "		  		         ${CYAN} Tu misión es hacerlos felices ${STD}"
 
 
 PREBE=`echo $(($RANDOM%22))`
 
+echo ""
+                                echo "" 
+echo ""
+                                echo "" 
 
 
+echo -e "						       ${RED}           \\\///              "
+echo -e "						               / _  _ \             "
+echo -e "		                                          ${RED}    (| (.)(.) |)           "
+echo -e "							.---.OOOo--()--oOOO.---.     "
+echo -e "							|                      |     "
+echo -e "							|  Ayudarás al Prebe:  |     "
+echo -e "							|         ${RED} $PREBE           |     "
+echo -e "							'---.oooO--------------'     "
+echo -e "							     (   )   Oooo.           "
+echo -e "							      \ (    (   )           "
+echo -e "						      	       \_)    ) /            "
+echo -e "						                     (_/             "
 
+#COMMENT
 
-echo -e "					         \\\///              "
-echo -e "					        / _  _ \             "
-echo -e "					      (| (.)(.) |)           "
-echo -e "					.---.OOOo--()--oOOO.---.     "
-echo -e "					|                      |     "
-echo -e "					|  Eres el prebebé     |     "
-echo -e "					|      $PREBE                |     "
-echo -e "					'---.oooO--------------'     "
-echo -e "					     (   )   Oooo.           "
-echo -e "					      \ (    (   )           "
-echo -e "					       \_)    ) /            "
-echo -e "					             (_/             "
-
-
-echo "Jugaras con el Prebebe $PREBE:"
-
-
-}
-
-
-clear 
-
-bordes(){
-tput setf 6
-   tput cup $MARGENSUP $MARGENIZQ
-   x=$MARGENIZQ
-   while [ "$x" -le "$UCOL" ];
-   do
-      printf %b "$BORDE"
-      x=$(( $x + 1 ));
-   done
-
-   # Draw sides
-   x=$MARGENSUP
-   while [ "$x" -le "$UFIL" ];
-   do
-      tput cup $x $MARGENIZQ; printf %b "$BORDE"
-      tput cup $x $UCOL; printf %b "$BORDE"
-      x=$(( $x + 1 ));
-   done
-
-   # Draw bottom
-   tput cup $UFIL $MARGENIZQ
-   x=$MARGENIZQ
-   while [ "$x" -le "$UCOL" ];
-   do
-      printf %b "$BORDE"
-      x=$(( $x + 1 ));
-   done
-   tput setf 9
-
-echo -e " \n " 
+#echo "Jugaras con el Prebebe $PREBE:"
+sleep 11s
 
 }
-
-ZEROES=`echo |awk '{printf("%0"'"$SNAKESIZE"'"d\n",$1)}' | sed 's/0/0 /g'`
-LASTPOSX=( $ZEROES )                    # Pad with zeroes to start with
-LASTPOSY=( $ZEROES )                    # Pad with zeroes to start with
-
-#SCORE=0                                 # Starting score
-
-clear
 prebes
-echo "
-Keys:
 
- W - ARRIBA
- S - ABAJO
- A - IZQUIERDA
- D - DERECHA
- X - SALIR
-
-
-
-
-Press Return to continue
-"
-stty -echo
-tput civis
-read RTN
-tput setb 0
-tput bold
-clear
-
-bordes
-#updatescore 0
-
+source rayo.sh
 

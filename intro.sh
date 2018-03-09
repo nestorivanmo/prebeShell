31f2f8ba7ecb020c78bad403509b7d8f60ddd0ac
barcenitas <erick3arcenas7@gmail.com>
2018-03-09 09:12:04 -0600

    agregue diseño

--- intro.sh
+++ intro.sh
@@ -1,42 +1,75 @@
 #!/bin/bash
 
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+STD='\033[0;0;39m'
+CYAN='\033[1;96m'
+IBlUE='\033[0;104m'
+WHITE="\033[1;97m"
+BLUE='\033[1;34m'
+
 intro()
 {
 clear
 for (( i=0; i<=5; i++));
 do
 clear
-echo -e "\n\n\t\t\e[$i;$i;$i;82mPreparando el descontrol . . .\e[0m"
-sleep 0.25;clear
-done
-echo -e "\n\n\n"
-echo -e "\e[93m                           *********          ********"
-echo -e "                           *   O   *          *  O   *"
-echo -e "                           *       *          *      *"
-echo -e "                           *       *          *      *"
-echo -e "\e[93m                  ***************************************      x  x x x x x"
-echo -e "                  * * * * * * \e[34m Welcome to our \e[93m  * * * * *    x   \e[5m HI! \e[93m     x"
-echo -e "                  *         \e[34m    PrebeShell   \e[93m           *   x              x"
-echo -e "                  *                                     *      x  x x x x "
-echo -e "                  * \e[0m Bárcenas ft. Martínez ft. Pineda \e[93m  *"
-echo -e "                  *                                     *"
-echo -e "                  * \e[0m       # 3      #11     #22   \e[93m      *"
-echo -e "                  *                                     *"
-echo -e "                  * | ||| ||||||   PROTECO  ||||||||| ||*"
-echo -e "                  ***************************************"
-echo -e "                            o o o o o  o oo o o o         l¬¬l"
-echo -e "                            o o o  x x x x xo o o       oo "
-echo -e "                            o o o o o o o o o o o     ooo"
-echo -e "                                o o o o o o o o     ooo"
-echo -e "                                o o o o o o o o  o o"
-echo -e "                                o o o o o o o o "
-echo -e "                                o o o o o o o o "
-echo -e "                                o o o o o o o o "
-echo -e "	         		   llll    lll   "
-echo -e "                                  llll    lll   "
-echo -e "                                  YYYYYY  YYYYYY "
+echo ""
+echo ""
+echo ""
+echo ""
 
-echo -e "\n\n\t\t\t\t\tPresiona cualquier tecla para comenzar..."
+echo -e "							${RED}\e[$i;$i;$i;82mPreparando el descontrol . . .\e[0m"
+sleep 0.25
+
+clear
+done
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "			\e[93m                           *********          ********"
+echo -e "                       			                           *   O   *          *  O   *"
+echo -e "                                                  *       *          *      *"
+echo -e "                                                  *       *          *      *"
+echo -e "                       \e[93m                  ***************************************      x  x x x x x"
+echo -e "                                         * * * * * * \e[34m Welcome to our \e[93m  * * * * *    x   \e[5m HI! \e[93m     x"
+echo -e "                                         *         \e[34m    PrebeShell   \e[93m           *   x              x"
+echo -e "                                         *                                     *      x  x x x x "
+echo -e "                                         * \e[0m Bárcenas ft. Martínez ft. Pineda \e[93m  *"
+echo -e "                                         *                                     *"
+echo -e "                                         * \e[0m       # 3      #11     #22   \e[93m      *"
+echo -e "                                         *                                     *"
+echo -e "                                         * | ||| ||||||   PROTECO  ||||||||| ||*"
+echo -e "                                         ***************************************"
+echo -e "                                                   o o o o o  o oo o o o         l¬¬l"
+echo -e "                                                   o o o  x x x x xo o o       oo "
+echo -e "                                                   o o o o o o o o o o o     ooo"
+echo -e "                                                       o o o o o o o o     ooo"
+echo -e "                                                       o o o o o o o o  o o"
+echo -e "                                                       o o o o o o o o "
+echo -e "                                                       o o o o o o o o "
+echo -e "                                                       o o o o o o o o "
+echo -e "	                                		   llll    lll   "
+echo -e "                                                         llll    lll   "
+echo -e "                                                         YYYYYY  YYYYYY "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo -e "      	                          	${GREEN} Presiona [cualquier tecla] para comenzar...";
 read
 
 }
